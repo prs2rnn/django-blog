075b026611dcbdfbfa1fc2ed453d54ba91571fb7
prs2rnn <developer.prs2rnn@gmail.com>
2026-06-08 01:41:52 +0500

    fix: main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,10 @@
 (function () {
   let mybutton = document.getElementById("btn-back-to-top");
 
+  if (mybutton === null) {
+    return;
+  }
+
   window.onscroll = function () {
     scrollFunction();
   };
