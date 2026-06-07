748dc70e7b28d88e8e8039c56508121486e446ee
prs2rnn <developer.prs2rnn@gmail.com>
2026-06-08 01:29:47 +0500

    feat: recent post

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from ..posts.models import Post
+
 
 class HomeView(TemplateView):
     template_name = "core/index.html"
@@ -8,6 +10,9 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         context["title"] = "Home".lower()
+        context["recent_posts"] = Post.objects.filter(is_published=True).order_by(
+            "-created_at"
+        )[:5]
 
         return context
 
