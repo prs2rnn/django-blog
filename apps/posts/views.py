96ea03c034b6a55b65544ef4713375b35e600139
prs2rnn <developer.prs2rnn@gmail.com>
2026-06-11 21:50:48 +0500

    feat: database search

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import DetailView, ListView
 
 from .models import Post
@@ -5,21 +6,30 @@ from .models import Post
 
 class PostListView(ListView):
     model = Post
-    template_name = "posts/post_list.html"
+    template_name = "posts/list.html"
     context_object_name = "posts"
 
     def get_queryset(self):
-        return Post.objects.filter(is_published=True)
+        query = self.request.GET.get("q", "")
+        posts = Post.objects.filter(is_published=True)
+
+        if query:
+            posts = posts.filter(
+                Q(title__icontains=query) | Q(content__icontains=query)
+            )
+
+        return posts.order_by("-created_at")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["query"] = self.request.GET.get("q", "")
         context["title"] = "Blog".lower()
         return context
 
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = "posts/post_detail.html"
+    template_name = "posts/detail.html"
     context_object_name = "post"
 
     def get_context_data(self, **kwargs):
