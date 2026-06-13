499699a6b9a1260b37354f05866344351cc250ce
prs2rnn <developer.prs2rnn@gmail.com>
2026-06-14 04:31:39 +0500

    fix: tag title

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -80,6 +80,7 @@ class TagPostListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["tag"] = self.tag
+        context["title"] = self.tag.name
         return context
 
 
